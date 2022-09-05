76efea1fab88b452d4e4bc64d9662b1131b7ff4a
ryanneilparker <ryanneilparker@gmail.com>
2022-09-05 18:45:07 +0000

    Add test cases

--- small_problems/reverse_it.rb
+++ small_problems/reverse_it.rb
@@ -9,3 +9,8 @@ p reverse_it('one two three') == 'three two one'
 p reverse_it('she did it') == 'it did she'
 p reverse_it('what where when') == 'when where what'
 
+puts reverse_it('Hello World') == 'World Hello'
+puts reverse_it('Reverse these words') == 'words these Reverse'
+puts reverse_it('') == ''
+puts reverse_it('    ') == '' # Any number of spaces results in ''
+
