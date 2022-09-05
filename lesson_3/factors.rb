151f9952a0a577ae97914c3aaea71d98aefef8be
ryanneilparker <ryanneilparker@gmail.com>
2022-09-05 12:08:49 +0000

    Add factors.rb

---
+++ lesson_3/factors.rb
@@ -0,0 +1,15 @@
+def factors(number)
+  divisor = number
+  factors = []
+
+  while divisor > 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+
+  factors
+end
+
+puts factors(10)
+puts factors(-10)
+puts factors(0)
