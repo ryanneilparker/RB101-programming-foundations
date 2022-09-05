33f9c3a224e9b40267172fd06241866eb974fecb
ryanneilparker <ryanneilparker@gmail.com>
2022-09-05 18:00:28 +0000

    Update odd.rb

--- small_problems/odd.rb
+++ small_problems/odd.rb
@@ -3,10 +3,13 @@
   This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
 =end
 
-def odd(integer)
-  integer.odd?
+def is_odd?(integer)
+  integer.abs % 2 != 0
 end
 
-puts odd(1)
-puts odd(0)
-puts odd(-4)
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
