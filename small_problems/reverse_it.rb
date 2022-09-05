dd518dc7ef067a0df212050ce8f15a8c17bb00a0
ryanneilparker <ryanneilparker@gmail.com>
2022-09-05 18:43:28 +0000

    Add reverse_it.rb

---
+++ small_problems/reverse_it.rb
@@ -0,0 +1,11 @@
+# Write a method that takes one argument, a string, and returns a new string with the words in reverse order.
+
+def reverse_it(sentence)
+  words = sentence.split(' ')
+  words.reverse.join(' ')
+end
+
+p reverse_it('one two three') == 'three two one'
+p reverse_it('she did it') == 'it did she'
+p reverse_it('what where when') == 'when where what'
+
