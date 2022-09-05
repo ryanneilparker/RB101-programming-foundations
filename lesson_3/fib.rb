84f43043e4a3b0c60d0ae9ea2ebd3e02006ea476
ryanneilparker <ryanneilparker@gmail.com>
2022-09-05 12:21:34 +0000

    Add fib.rb

---
+++ lesson_3/fib.rb
@@ -0,0 +1,10 @@
+def fibo(first_num, second_num, limit)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+puts fibo(1, 2, 100)
