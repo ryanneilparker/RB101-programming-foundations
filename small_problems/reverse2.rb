57620239a87fcc24528ffa0d2c9041997985cf69
ryanneilparker <ryanneilparker@gmail.com>
2022-09-05 18:56:22 +0000

    Add reverse2.rb

---
+++ small_problems/reverse2.rb
@@ -0,0 +1,17 @@
+# Write a method that takes one argument, a string containing one or more words, 
+# and returns the given string with words that contain five or more characters reversed. 
+# Each string will consist of only letters and spaces. 
+# Spaces should be included only when more than one word is present.
+
+def reverse_words(string)
+  words = string.split(' ')
+  words.each do |word|
+    word.reverse! if word.length >= 5
+  end
+  words.join(' ')
+end
+
+
+puts reverse_words('Professional') == 'lanoisseforP'
+puts reverse_words('Walk around the block') == 'Walk dnuora the kcolb'
+puts reverse_words('Launch School') == 'hcnuaL loohcS'
