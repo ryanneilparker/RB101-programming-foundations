5b2ee0ef458fd39a4d63324168f380774ea6120c
ryanneilparker <ryanneilparker@gmail.com>
2022-09-06 19:11:09 +0000

    Add flint.rb

---
+++ lesson_4/flint.rb
@@ -0,0 +1,9 @@
+# Given the array below
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+
+# Turn this array into a hash where the names are the keys and the values are the positions in the array.
+
+hash = {}
+flintstones.each_with_index { |name, index| hash[name] = index }
+p hash
