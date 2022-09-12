be507ec02e0250c812796c9b60dca7523a7ebfd7
ryanneilparker <ryanneilparker@gmail.com>
2022-09-12 18:08:02 +0000

    Add prob8.rb

---
+++ lesson_5/prob8.rb
@@ -0,0 +1,26 @@
+# Using the each method, write some code to output all of the vowels from the strings.
+
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
+
+# loop hrough hash and extract words
+words =[]
+hsh.each do |key, _|
+  words << hsh[key]
+end
+words.flatten!
+p words
+
+# Loop through words and extract letters
+letters = []
+words.each do |word|
+  letters << word.split('')
+end
+letters.flatten!
+p letters
+
+# Add letters to result if they are vowels
+vowels = []
+letters.each do |letter|
+  vowels << letter if letter =~ /[aeiou]/
+end
+p vowels
