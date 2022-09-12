68128b03ee1253490cb26a208469b6ce905ff051
ryanneilparker <ryanneilparker@gmail.com>
2022-09-12 20:15:55 +0000

    Add sub.rb

---
+++ lesson_5/sub.rb
@@ -0,0 +1,14 @@
+# Given the following data structure, and without using the Array#to_h method, 
+# write some code that will return a hash where the key is the first item in each sub array and the value is the second item.
+
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+# expected return value: {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
+
+new_hsh = {}
+
+arr.each do |sub_arr|
+  new_hsh[sub_arr[0]] = sub_arr[1]
+end
+
+p arr
+p new_hsh == {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
