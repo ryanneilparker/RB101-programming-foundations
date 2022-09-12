6386f0c65537621229c537cc0fb606a2d44f8611
ryanneilparker <ryanneilparker@gmail.com>
2022-09-12 20:10:37 +0000

    Add int.rb

---
+++ lesson_5/int.rb
@@ -0,0 +1,14 @@
+# Given the following data structure use a combination of methods, 
+# including either the select or reject method, 
+# to return a new array identical in structure to the original 
+# but containing only the integers that are multiples of 3.
+
+arr = [[2], [3, 5, 7], [9], [11, 13, 15]]
+
+new_arr = arr.map do |sub_arr|
+  sub_arr.reject { |val| val % 3 != 0}
+end
+
+p arr
+p new_arr
+
