b279db425c2202dc38f11404b496e5409603451c
ryanneilparker <ryanneilparker@gmail.com>
2022-09-21 18:06:37 +0000

    Addit all

--- small_problems/easy_2/greetings.rb
+++ small_problems/easy_2/greetings.rb
@@ -2,7 +2,7 @@ puts "What is your name?"
 name = gets.chomp
 
 if name[-1] == '!'
-  puts "HELLO #{name}! WHY ARE WE SCREAMING?!"
+  puts "HELLO #{name.upcase}! WHY ARE WE SCREAMING?!"
 else
   puts "Hello #{name}."
 end
