d4ff1bcb1164c85def895898b4b46e8fbb38f34b
ryanneilparker <ryanneilparker@gmail.com>
2022-09-21 17:48:48 +0000

    Add Time class from std lib

--- small_problems/easy_2/when_will_I_retire?.rb
+++ small_problems/easy_2/when_will_I_retire?.rb
@@ -4,8 +4,9 @@ age = gets.chomp.to_i
 puts "At what age would you like to retire?"
 retirement_age = gets.chomp.to_i
 
+year = Time.now.year
 time = (retirement_age - age)
-date = 2022 + time
+date = year + time
 
-puts "It's 2022 you will retire in #{date}."
+puts "It's #{year} you will retire in #{date}."
 puts "You have only #{time} years of work to go!"
