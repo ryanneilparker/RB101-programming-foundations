647ff6d3df7b0605b3dc2615944f6f0bee3506f4
ryanneilparker <ryanneilparker@gmail.com>
2022-09-22 18:37:01 +0000

    Add julian calendar

--- small_problems/easy_4/leap.rb
+++ small_problems/easy_4/leap.rb
@@ -44,6 +44,18 @@ end
 =end
 
 def leap_year?(year)
+  if year >= 1752
+    gregorian_leap_year?(year)
+  else
+    julian_leap_year?(year)
+  end
+end
+
+def julian_leap_year?(year)
+  year % 4 == 0
+end
+
+def gregorian_leap_year?(year)
   if year % 4 == 0 && year % 100 != 0
     true
   elsif year % 100 == 0 && year % 400 == 0 
@@ -62,7 +74,7 @@ p leap_year?(240001) == false
 p leap_year?(2000) == true
 p leap_year?(1900) == false
 p leap_year?(1752) == true
-p leap_year?(1700) == false
+p leap_year?(1700) == true
 p leap_year?(1) == false
-p leap_year?(100) == false
+p leap_year?(100) == true
 p leap_year?(400) == true
