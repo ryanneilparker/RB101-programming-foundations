f111d5144552b1d7934aef08d765b653fa67991d
ryanneilparker <ryanneilparker@gmail.com>
2022-10-03 14:23:13 +0000

    Add consonant feature

--- small_problems/easy_8/double.rb
+++ small_problems/easy_8/double.rb
@@ -6,17 +6,25 @@ Write a method that takes a string, and returns a new string in which every char
 
 require 'pry'
 
-def repeater(string)
+CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z) + %w(b c d f g h j k l m n p q r s t v w x y z).map(&:upcase)
+
+def consonant?(char)
+  CONSONANTS.include?(char)
+end
+
+def double_consonants(string)
   chars = string.chars
   new_chars = []
 
   chars.each do |char|
-    new_chars << char << char
+    new_chars << char << char if consonant?(char)
+    new_chars << char unless consonant?(char)
   end
 
   new_chars.join
 end
 
-p repeater('Hello') == "HHeelllloo"
-p repeater("Good job!") == "GGoooodd  jjoobb!!"
-p repeater('') == ''
+p double_consonants('String') == "SSttrrinngg"
+p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
+p double_consonants("July 4th") == "JJullyy 4tthh"
+p double_consonants('') == ""
