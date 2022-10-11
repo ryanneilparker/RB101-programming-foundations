e0c24a7f016533cb1fa8551ba85e73dc3d65ad1b
ryanneilparker <ryanneilparker@gmail.com>
2022-10-11 18:20:14 +0000

    Add test cases

--- small_problems/advanced_1/merge_sort.rb
+++ small_problems/advanced_1/merge_sort.rb
@@ -49,32 +49,35 @@ new_arr
 
 require 'pry'
 
-def merge(arr1, arr2)
-  new_arr = []
-  arr1_index = 0
-  arr2_index = 0
+def merge(array1, array2)
+  index2 = 0
+  result = []
 
-  if arr1.empty? || arr2.empty?
-    return (arr1 + arr2).flatten
-  end
-
-  loop do
-    if arr2[arr2_index] == nil || arr1[arr1_index] < arr2[arr2_index]
-      new_arr << arr1[arr1_index]
-      arr1_index += 1
-    elsif arr1[arr1_index] == nil || arr2[arr2_index] < arr1[arr1_index]
-      new_arr << arr2[arr2_index]
-      arr2_index += 1
+  array1.each do |value|
+    while index2 < array2.size && array2[index2] <= value
+      result << array2[index2]
+      index2 += 1
     end
-
-    break if arr1_index > [arr1.size, arr2.size].max || arr2_index > [arr1.size, arr2.size].max
+    result << value
   end
 
-  new_arr.delete(nil)
-  new_arr
+  result.concat(array2[index2..-1])
 end
 
-p merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
-p merge([1, 1, 3], [2, 2]) == [1, 1, 2, 2, 3]
-p merge([], [1, 4, 5]) == [1, 4, 5]
-p merge([1, 4, 5], []) == [1, 4, 5]
+def merge_sort(array)
+  return array if array.size == 1
+
+  sub_array_1 = array[0...array.size / 2]
+  sub_array_2 = array[array.size / 2...array.size]
+
+  sub_array_1 = merge_sort(sub_array_1)
+  sub_array_2 = merge_sort(sub_array_2)
+
+  merge(sub_array_1, sub_array_2)
+end
+
+p merge_sort([9, 5, 7, 1]) == [1, 5, 7, 9]
+p merge_sort([5, 3]) == [3, 5]
+p merge_sort([6, 2, 7, 1, 4]) == [1, 2, 4, 6, 7]
+p merge_sort(%w(Sue Pete Alice Tyler Rachel Kim Bonnie)) == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+p merge_sort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]) == [1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
