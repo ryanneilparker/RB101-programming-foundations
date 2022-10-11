cce047480ee344d3690e33f06292b8345f415d29
ryanneilparker <ryanneilparker@gmail.com>
2022-10-11 17:45:22 +0000

    Add mxn suport

--- small_problems/advanced_1/transpose.rb
+++ small_problems/advanced_1/transpose.rb
@@ -57,3 +57,30 @@ new_matrix = transpose(matrix)
 
 p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
 p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
+
+=begin
+Transpose MxN
+In the previous exercise, you wrote a method to transpose a 3 x 3 matrix as represented by a ruby Array of Arrays.
+
+Matrix transposes aren't limited to 3 x 3 matrices, or even square matrices. Any matrix can be transposed by simply switching columns with rows.
+
+Modify your transpose method from the previous exercise so it works with any matrix with at least 1 row and 1 column.
+=end
+
+def transpose_mxn(matrix)
+  result = []
+  number_of_rows = matrix.size
+  number_of_columns = matrix.first.size
+
+  (0...number_of_columns).each do |column_index|
+    new_row = (0...number_of_rows).map { |row_index| matrix[row_index][column_index] }
+    result << new_row
+  end
+  
+  result
+end
+
+p transpose_mxn([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+p transpose_mxn([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+p transpose_mxn([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) == [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+p transpose_mxn([[1]]) == [[1]]
