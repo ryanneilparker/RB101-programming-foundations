3afc55ef35f548bb8695e72a5b6d90b8a70349b5
ryanneilparker <ryanneilparker@gmail.com>
2022-10-11 17:34:00 +0000

    Add transpose.rb

---
+++ small_problems/advanced_1/transpose.rb
@@ -0,0 +1,59 @@
+=begin
+Transpose 3x3
+Write a method that takes a 3 x 3 matrix in Array of Arrays format 
+and returns the transpose of the original matrix. 
+Note that there is a Array#transpose method that does this -- you may not use it for this exercise. 
+You also are not allowed to use the Matrix class from the standard library. 
+Your task is to do this yourself.
+
+Take care not to modify the original matrix: 
+you must produce a new matrix and leave the original matrix unchanged.
+
+PROBLEM
+inputs: nested array (3x3 matrix)
+outputs: nested array (3x3 matrix)
+requirements: transpose the array by swapping the rows and columns
+questinos: mutate? (no), always int (yes), empty? (no)
+
+DATA
+row array: matrix[1]
+column array: matrix[1][0] + matrix[2][0] + matrix[3][0]
+
+ALGO
+extract each column of the matrix into a new array
+stitch the arrays together to form a transposed matrix
+
+PSEUDO
+transposed_matrix = []
+for col in 0..2
+  for row in 0..2
+    transposed_matrix[col][row] = matrix[row][col]
+  end
+end
+transposed_matrix
+=end
+
+require 'pry'
+
+def transpose(matrix)
+transposed_matrix = [[],[],[]]
+
+for col in 0..2
+  for row in 0..2
+    transposed_matrix[col][row] = matrix[row][col]
+  end
+end
+
+transposed_matrix
+end
+
+matrix = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6]
+]
+
+new_matrix = transpose(matrix)
+
+p new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+p matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]]
